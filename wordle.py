64856f84e696d5b9bad09f0ad33bd4f01ce1a9f1
SmashingPrograms <smashingprograms@gmail.com>
2022-02-09 14:33:02 -0500

    Word data grab and sort

--- wordle.py
+++ wordle.py
@@ -1,3 +1,26 @@
+def load_words():
+  with open('/usr/share/dict/words') as infile:
+    word_list = infile.read().splitlines()
+  return word_list
+
+old_word_list = load_words()
+
+word_list = []
+
+count = 0
+
+for word in old_word_list:
+  count += 1
+  print("Loading word ", count, " of ", len(old_word_list))
+  if len(word) == 5:
+    word_list.append(word.upper())
+
+print("\n\nLoaded!\n\n")
+
+
+
+
+
 def green_background(str):
   return "\33[7;49;32m" + str + "\33[0;49;37m"
 
@@ -7,8 +30,13 @@ def gray_background(str):
 def yellow_background(str):
   return "\33[7;49;93m" + str + "\33[0;49;37m"
 
-print(yellow_background("Hey!"))
-print(gray_background("Hey!"))
-print(green_background("Hey!"))
 
-print("Hello")
+# list_of_words = []
+# for word in old_word_list:
+#   print(word)
+#   print(len(word))
+#   if len(word) == 5:
+#     list_of_words.append(word)
+
+# print(list_of_words)
+# print(len(list_of_words))
